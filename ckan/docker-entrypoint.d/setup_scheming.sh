36e94799966eeb791e657a897b71e61d35344526
Hans-Chrstian <hanschristian.vanderwerf@health-ri.nl>
2025-02-11 17:08:27 +0100

    Onlky focus HealthDCAT

--- ckan/docker-entrypoint.d/setup_scheming.sh
+++ ckan/docker-entrypoint.d/setup_scheming.sh
@@ -7,7 +7,7 @@
 # Update the config file with each extension config-options
 echo "[ckanext-scheming] Setting up config-options"
 ckan config-tool $CKAN_INI -s app:main \
-    "scheming.dataset_schemas = ckanext.dcat.schemas:health_dcat_ap.yaml ckanext.gdi_userportal:scheming/schemas/gdi_userportal.yaml ckanext.dataset_series.schemas:dcat_ap_dataset_series.yaml" \
+    "scheming.dataset_schemas = ckanext.dcat.schemas:health_dcat_ap.yaml ckanext.gdi_userportal:scheming/schemas/gdi_userportal.yaml" \
     "scheming.presets = ckanext.scheming:presets.json ckanext.dcat.schemas:presets.yaml ckanext.gdi_userportal:scheming/presets/gdi_presets.yaml" \
     "scheming.dataset_fallback = false" \
     "ckanext.dcat.rdf.profiles = euro_health_dcat_ap euro_dcat_ap_scheming fairdatapoint_dcat_ap" \
