eb05671af517e051f1a13fc66eda445649761428
Hans-christian <hanschristian.vanderwerf@health-ri.nl>
2025-06-19 17:00:45 +0200

    feat(dataseries) initial commit dataseries

--- ckan/docker-entrypoint.d/setup_scheming.sh
+++ ckan/docker-entrypoint.d/setup_scheming.sh
@@ -7,8 +7,8 @@
 # Update the config file with each extension config-options
 echo "[ckanext-scheming] Setting up config-options"
 ckan config-tool $CKAN_INI -s app:main \
-    "scheming.dataset_schemas = ckanext.dcat.schemas:health_dcat_ap.yaml ckanext.gdi_userportal:scheming/schemas/gdi_userportal.yaml" \
-    "scheming.presets = ckanext.scheming:presets.json ckanext.dcat.schemas:presets.yaml ckanext.gdi_userportal:scheming/presets/gdi_presets.yaml" \
+    "scheming.dataset_schemas = ckanext.dcat.schemas:health_dcat_ap.yaml ckanext.gdi_userportal:scheming/schemas/gdi_userportal.yaml ckanext.dcat.schemas:dcat_ap_dataset_series.yaml" \
+    "scheming.presets = ckanext.scheming:presets.json ckanext.dcat.schemas:presets.yaml ckanext.gdi_userportal:scheming/presets/gdi_presets.yaml ckanext.dataset_series.schemas:presets.yaml" \
     "scheming.dataset_fallback = false" \
     "ckanext.dcat.rdf.profiles = euro_health_dcat_ap euro_dcat_ap_scheming fairdatapoint_dcat_ap" \
     "ckanext.dcat.compatibility_mode = false"
