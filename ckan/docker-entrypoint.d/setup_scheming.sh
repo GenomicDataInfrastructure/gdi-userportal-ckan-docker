456f451d1d708cacf047b9ed78ed782efb948039
Hans-christian <hanschristian.vanderwerf@health-ri.nl>
2025-07-17 21:59:05 +0200

    Patch extensions

--- ckan/docker-entrypoint.d/setup_scheming.sh
+++ ckan/docker-entrypoint.d/setup_scheming.sh
@@ -10,7 +10,7 @@ ckan config-tool $CKAN_INI -s app:main \
     "scheming.dataset_schemas = ckanext.dcat.schemas:health_dcat_ap.yaml ckanext.gdi_userportal:scheming/schemas/gdi_userportal.yaml ckanext.dcat.schemas:dcat_ap_dataset_series.yaml" \
     "scheming.presets = ckanext.scheming:presets.json ckanext.dcat.schemas:presets.yaml ckanext.gdi_userportal:scheming/presets/gdi_presets.yaml ckanext.dataset_series.schemas:presets.yaml" \
     "scheming.dataset_fallback = false" \
-    "ckanext.dcat.rdf.profiles = euro_health_dcat_ap euro_dcat_ap_scheming fairdatapoint_dcat_ap" \
+    "ckanext.dcat.rdf.profiles = fairdatapoint_dcat_ap" \
     "ckanext.dcat.compatibility_mode = false"
 
 
